2c040b6200ce135811d174593382c5fdc0752df4
Brandon Chambers <bran.cham@gmail.com>
2022-07-15 12:35:52 -0700

    SITE:LeetCode|NAME:isSubsequence|DS:String|DIFF:Easy|TIME:30m00s|SOLUTION:NO

---
+++ leetcode/strings/isSubsequence.js
@@ -0,0 +1,43 @@
+/*
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+
+*/
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    if(s.length === 0) return true
+    let sPtr = 0, tPtr = 0, i = 0
+
+    while( sPtr < s.length && tPtr < t.length){
+        if( s[sPtr] === t[tPtr] ) {
+            sPtr+=1
+            if( sPtr === s.length ) return true
+        }
+        tPtr+=1
+    }
+    return false
+};
+
+console.log(isSubsequence("abc","ahbgdc"))
+console.log(isSubsequence("axc","ahbgdc"))
+console.log(isSubsequence("rjufvjafbxnbgriwgokdgqdqewn","mjmqqjrmzkvhxlyruonekhhofpzzslupzojfuoztvzmmqvmlhgqxehojfowtrinbatjujaxekbcydldglkbxsqbbnrkhfdnpfbuaktupfftiljwpgglkjqunvithzlzpgikixqeuimmtbiskemplcvljqgvlzvnqxgedxqnznddkiujwhdefziydtquoudzxstpjjitmiimbjfgfjikkjycwgnpdxpeppsturjwkgnifinccvqzwlbmgpdaodzptyrjjkbqmgdrftfbwgimsmjpknuqtijrsnwvtytqqvookinzmkkkrkgwafohflvuedssukjgipgmypakhlckvizmqvycvbxhlljzejcaijqnfgobuhuiahtmxfzoplmmjfxtggwwxliplntkfuxjcnzcqsaagahbbneugiocexcfpszzomumfqpaiydssmihdoewahoswhlnpctjmkyufsvjlrflfiktndubnymenlmpyrhjxfdcq"))
